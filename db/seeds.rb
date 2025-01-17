5e8d18874883fcff96d18c78018c899e1ac62495
Eliot Smith <eliotsmith19@gmail.com>
2025-01-17 17:06:29 +0000

    Added better rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
 puts "Creating cars..."
 
-# Owner.destroy_all
-# Car.destroy_all
+Car.destroy_all
 
-# Owner.create!(nickname: "Smithy")
-# Owner.create!(nickname: "Matty")
-# Owner.create!(nickname: "L-dot")
+# Owner.create!(nickname: "Smithy" 13)
+# Owner.create!(nickname: "Matty" 14 )
+# Owner.create!(nickname: "L-dot" 15)
 
-Car.create!(brand: "Nissan", model: "Skyline", year: 2015, fuel: "Petrol", owner_id: 13)
-Car.create!(brand: "MG", model: "ZS", year: 2022, fuel: "Petrol", owner_id: 14)
-Car.create!(brand: "BMW", model: "M4", year: 2018, fuel: "Diesel", owner_id: 15)
+Car.create!(brand: "Nissan", model: "Skyline", year: 2015, fuel: "Petrol", owner_id: 13, image_url: "https://cimg0.ibsrv.net/ibimg/hgm/1920x1080-1/100/879/r34-nissan-skyline-gt-r-from-fast-and-furious-4--photo-credit-bonhams_100879624.jpg")
+Car.create!(brand: "McLaren", model: "P1", year: 2022, fuel: "Petrol", owner_id: 14, image_url: "https://hips.hearstapps.com/hmg-prod/images/mclaren-p1-hybrid-hypercar1-1519828656.jpg?crop=0.9215854006273168xw:1xh;center,top&resize=1200:*")
+Car.create!(brand: "Toyota", model: "Supra", year: 2023, fuel: "Petrol", owner_id: 15, image_url: "https://www.the-lowdown.com/wp-content/uploads/2023/11/street-element-toyora-supra-front-quarter.jpg")
 
 puts "Created #{Owner.count} owners & #{Car.count} cars!"
